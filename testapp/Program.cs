3cd5229f45a3e882a9d518507f789e8e49ac55ce
dotnetdude <suresh4job@gmail.com>
2016-07-13 23:41:18 +0100

    dotnetcore_spa
    playing with dotnetcore spa

---
+++ testapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var test = "hello";
+            Console.WriteLine(test);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
